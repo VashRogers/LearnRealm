303b5ee19d47b3cf05fbabed84e7ddf6efac2624
VashRogers <rogerssj2@gmail.com>
2022-10-31 22:56:26 -0400

    Fazendo primeira tela

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Routes from "./src/routes";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   
 	return(
-		<Routes/>
+		<>
+			<StatusBar backgroundColor="transparent" translucent={true} barStyle="light-content" />
+			<Routes/>
+		</>
 	)
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,38 @@
 import React from "react";
-import { View, Text } from "react-native";
+// import { View, Text } from "react-native";
+import { Container, Title, Form, Input, Submit } from "./styles";
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Main(props) {
     
     
     return(
-        <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
-            <Text>Teste</Text>
-        </View>
+        <Container>
+            <Title>Repositórios</Title>
+            <Form>
+                <Input
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    placeholder="Procurar Repositório..."
+
+                />
+                <Submit
+                    onPress={() => {
+
+                    }}
+                >
+                    <MaterialIcons name="add" size={22} color="#ffff" />
+                </Submit>
+            </Form>
+            
+            <List
+                keyboardShouldPersistTaps="handled"
+                data={[]}
+                keyExtractor={item => String(item.id)}
+                renderItem={({ item }) => {
+                    
+                }}
+            />
+        </Container>
     )
 }
